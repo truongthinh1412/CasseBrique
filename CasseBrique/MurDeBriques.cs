20d2cdae7304be8d3ac4043d1bc4ee31102c36d8
emaudealtema <32494569+emaudealtema@users.noreply.github.com>
2020-12-26 22:52:50 +0700

    Update MurDeBriques.cs
    Emaude

--- CasseBrique/MurDeBriques.cs
+++ CasseBrique/MurDeBriques.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CasseBrique
 {
@@ -9,6 +7,7 @@ namespace CasseBrique
     {
         public List<Brique> briques;
         public int nbBriques = 10;
+        public int nbBriqueVitesse = 5;
 
 
         public MurDeBriques(Game game) : base(game)
@@ -21,6 +20,14 @@ namespace CasseBrique
             for (int i = 0; i < this.nbBriques; i++)
             {
                 Brique brique = new Brique(base.Game, "Textures/wall");
+                briques.Add(brique);
+                this.Game.Components.Add(brique);
+            }
+
+            
+            for (int i = 0; i < this.nbBriqueVitesse; i++)
+            {
+                BriqueDeVitesse brique = new BriqueDeVitesse(base.Game, "Textures/wall");
                 briques.Add(brique);
                 this.Game.Components.Add(brique);
             }
